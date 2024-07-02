799cac93ed66df3099b37e278d0bcc81beb9d7e8
Jasraj Singh <elijahbaggins3@gmail.com>
2024-07-02 22:56:33 +0100

    change api url to have https instead of http

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://104.248.165.232/api',
-    weatherUrl: 'http://104.248.165.232/WeatherForecast'
+    apiUrl: 'https://104.248.165.232/api',
+    weatherUrl: 'https://104.248.165.232/WeatherForecast'
 };
