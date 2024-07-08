cb5bc1a9173ae6b6ee1f5e510d6abf931fbe14eb
Jasraj Singh <elijahbaggins3@gmail.com>
2024-07-08 02:07:14 +0100

    Updated api url to new domain name apitodo.jassingh.me

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://104.248.165.232/api',
-    weatherUrl: 'https://104.248.165.232/WeatherForecast'
+    apiUrl: 'https://apitodo.jassingh.me/api',
+    weatherUrl: 'https://apitodo.jassingh.me/WeatherForecast'
 };
